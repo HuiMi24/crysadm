6c51fc8c94639a2c780d05729745dc116962a2d8
Mi Hui <mihui24@gmail.com>
2016-02-29 11:06:00 +0800

    change to ProductionConfig mode

--- crysadm.py
+++ crysadm.py
@@ -14,8 +14,8 @@ elif socket.gethostname() == 'iZ23bo17lpkZ':
 else:
     app.config.from_object(config.TestingConfig)
 """
-conf = config.TestingConfig
-app.config.from_object(config.TestingConfig)
+conf = config.ProductionConfig
+app.config.from_object(config.ProductionConfig)
 redis_conf = app.config.get('REDIS_CONF')
 pool = redis.ConnectionPool(host=redis_conf.host, port=redis_conf.port, db=redis_conf.db, password=redis_conf.password)
 r_session = redis.Redis(connection_pool=pool)

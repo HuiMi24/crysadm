647acb6b4ad121a0e0a0093c7a9088a2b7fae73f
Hui Mi <Hui Mi>
2016-04-03 21:39:11 +0800

    update log

--- excavator.py
+++ excavator.py
@@ -55,21 +55,18 @@ def regular_html(info):
 def red_log(clas, type, id, gets):
     user = session.get('user_info')
 
-    user_key = '%s:%s' % ('user', user.get('username'))
-    user_info = json.loads(r_session.get(user_key).decode('utf-8'))
-
-    if user_info.get('log_as_body') is None:
-        user_info['log_as_body'] = []
+    record_key = '%s:%s' % ('record', user.get('username'))
+    record_info = json.loads(r_session.get(record_key).decode('utf-8'))
 
     log_as_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     body = dict(time=log_as_time, clas=clas, type=type, id=id, gets=gets)
 
-    log_as_body = user_info.get('log_as_body')
+    log_as_body = record_info.get('diary')
     log_as_body.append(body)
 
-    user_info['log_as_body'] = log_as_body
+    record_info['diary'] = log_as_body
 
-    r_session.set(user_key, json.dumps(user_info))
+    r_session.set(record_key, json.dumps(record_info))
 
 # 收取水晶[id]
 @app.route('/collect/<user_id>', methods=['POST'])
--- log.py
+++ log.py
@@ -11,24 +11,30 @@ def user_log():
     log_as = []
     user = session.get('user_info')
 
-    user_key = '%s:%s' % ('user', user.get('username'))
-    user_info = json.loads(r_session.get(user_key).decode('utf-8'))
+    record_key = '%s:%s' % ('record', user.get('username'))
+    record_info = json.loads(r_session.get(record_key).decode('utf-8'))
 
-    if user_info.get('log_as_body') is None:
-        user_info['log_as_body'] = []
-
-    for row in user_info.get('log_as_body'):
+    for row in record_info.get('diary'):
         if (datetime.now() - datetime.strptime(row.get('time'), '%Y-%m-%d %H:%M:%S')).days < 7:
             log_as.append(row)
     log_as.reverse()
 
     return render_template('log.html', log_user=log_as)
 
+
 @app.route('/log/delete')
 @requires_auth
 def user_log_delete():
     user = session.get('user_info')
 
+    record_key = '%s:%s' % ('record', user.get('username'))
+    record_info = json.loads(r_session.get(record_key).decode('utf-8'))
+
+    record_info['diary'] = []
+
+    r_session.set(record_key, json.dumps(record_info))
+
+    return redirect(url_for('user_log'))
     user_key = '%s:%s' % ('user', user.get('username'))
     user_info = json.loads(r_session.get(user_key).decode('utf-8'))
 

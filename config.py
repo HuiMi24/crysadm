42f504af2062b46d1ed8d611eb949687e17f75e0
Mi Hui <mihui24@gmail.com>
2016-05-27 13:11:39 +0800

    change log function

--- config.py
+++ config.py
@@ -7,21 +7,10 @@ import logging
 
 LOG_FILENAME = '/tmp/error'
 
-logging.basicConfig(level=logging.DEBUG)
 def crys_log(message):
     """Automatically log the current function details."""
     import inspect, logging
-    # Get the previous frame in the stack, otherwise it would
-    # be this function!!!
-    func = inspect.currentframe().f_back.f_code
-    # Dump the message + the name of this function to the log.
-    logging.debug("%s: %s in %s:%i" % (
-        message,
-        func.co_name,
-        func.co_filename,
-        func.co_firstlineno
-    ))
-
+    print(message)
 crys_log("This is a debug message")
 
 class RedisConfig():

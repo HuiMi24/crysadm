7a500480e84fc0834a589f3c47ecf47c9c6df7af
Mi Hui <mihui24@gmail.com>
2016-06-08 10:18:03 +0800

    add script run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ps -ef |grep -E 'crysadm'|grep -v grep|awk '{print $2}'|xargs sudo kill -9
+
+BASE_DIR="/var/www/crysadm"
+
+python3.4 ${BASE_DIR}/crysadm_helper.py >> /var/log/uwsgi/crysadm_uwsgi.log 2>&1 &
+uwsgi --ini /var/www/crysadm/crysadm_uwsgi.ini >> /var/log/uwsgi/crysadm_uwsgi.log 2>&1 &

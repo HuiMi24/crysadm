2dcd26e6ef142bc2e1c6892a8c2faf976c310740
HuiMi24 <mihui24@gmail.com>
2017-05-17 17:01:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ ps -ef |grep -E 'crysadm'|grep -v grep|awk '{print $2}'|xargs sudo kill -9
 
 BASE_DIR="/var/www/crysadm"
 
-python3.4 ${BASE_DIR}/crysadm_helper.py >> /var/log/uwsgi/crysadm_uwsgi.log 2>&1 &
-uwsgi --ini /var/www/crysadm/crysadm_uwsgi.ini >> /var/log/uwsgi/crysadm_uwsgi.log 2>&1 &
+python3 ${BASE_DIR}/crysadm.py >> /var/log/crysadm.log 2>&1 &
+python3 ${BASE_DIR}/crysadm_helper.py >> /var/log/crysadm.log 2>&1 &
